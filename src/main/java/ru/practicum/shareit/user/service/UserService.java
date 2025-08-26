f35035aab360eccf5b809ce2bc623c5842fe8ff6
Evgeny Uglov <evguglov@yandex.ru>
2025-08-26 08:05:30 +0400

    fix: EmptyLineSeparator

--- src/main/java/ru/practicum/shareit/user/service/UserService.java
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -5,9 +5,14 @@ import ru.practicum.shareit.user.dto.UserDto;
 import java.util.List;
 
 public interface UserService {
+
     UserDto create(UserDto dto);
+
     UserDto update(Long id, UserDto dto);
+
     UserDto getById(Long id);
+
     List<UserDto> getAll();
+
     void delete(Long id);
 }
